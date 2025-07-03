7ba5814690b7591fb49cf89b93496a6510b535d0
NijhuisSven <svennijhuis1976@gmail.com>
2025-07-03 17:44:11 +0200

    Update RegionAPI.java

--- src/main/java/nl/itsmejasperr/regionAPI/RegionAPI.java
+++ src/main/java/nl/itsmejasperr/regionAPI/RegionAPI.java
@@ -10,29 +10,5 @@ public final class RegionAPI extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new RegionListener(), this);
-
-
-        long start = System.currentTimeMillis();
-
-        getLogger().info("========[ RegionAPI ]========");
-        getLogger().info("Status            : ENABLED");
-        getLogger().info("Version           : " + getDescription().getVersion());
-        getLogger().info("Author(s)         : " + String.join(", ", getDescription().getAuthors()));
-        getLogger().info("Function          : Region enter tracking");
-        getLogger().info("WorldGuard Hook   : " + (Bukkit.getPluginManager().isPluginEnabled("WorldGuard") ? "Found" : "Missing"));
-        getLogger().info("Listener Registered: " + (HandlerList.getRegisteredListeners(this).size() > 0 ? "Yes" : "No"));
-        getLogger().info("Load Time         : " + (System.currentTimeMillis() - start) + " ms");
-        getLogger().info("GitHub            : github.com/ItsMeJasperr/RegionAPI");
-        getLogger().info("Discord Support   : discord.itsmejasperr.nl");
-        getLogger().info("=============================");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("========[ RegionAPI ]========");
-        getLogger().info("Status            : DISABLED");
-        getLogger().info("GitHub            : github.com/ItsMeJasperr/RegionAPI");
-        getLogger().info("Discord Support   : discord.itsmejasperr.nl");
-        getLogger().info("=============================");
     }
 }
